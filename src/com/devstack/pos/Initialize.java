c2a355ffc5a41d743b5a6fafba3d85a3f8e9fd56
Shashika <washa@gmail.com>
2023-08-22 00:53:26 +0530

    Initial commit

---
+++ src/com/devstack/pos/Initialize.java
@@ -0,0 +1,22 @@
+package com.devstack.pos;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Initialize extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.
+                load(getClass().getResource("LoginForm.fxml"))));
+        primaryStage.show();
+    }
+}
---
+++ src/com/devstack/pos/controller/LoginFormController.java
@@ -0,0 +1,5 @@
+package com.devstack.pos.controller;
+
+public class LoginFormController {
+
+}
